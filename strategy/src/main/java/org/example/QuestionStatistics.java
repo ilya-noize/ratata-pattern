209fe9366607b938d885c06d0196673d9c3e508c
Ilya Shumakov <shumakoff.ilya@yandex.ru>
2026-01-28 23:41:56 +0300

    feat: Strategy:template

---
+++ strategy/src/main/java/org/example/QuestionStatistics.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.Map;
+
+public class QuestionStatistics {
+    private String questionTitle;
+    private Map<String,Integer> selectedVariantsCount;
+    private Map<String,Integer> userSelectedVariantsCount;
+}
---
+++ strategy/src/main/java/org/example/strategy/FullCountStrategy.java
@@ -0,0 +1,14 @@
+package org.example.strategy;
+
+import org.example.AnalyzeStrategy;
+import org.example.QuestionStatistics;
+
+import java.util.List;
+
+public class FullCountStrategy implements AnalyzeStrategy {
+
+    @Override
+    public void makeAnalyze(List<QuestionStatistics> questionStatistics) {
+
+    }
+}
