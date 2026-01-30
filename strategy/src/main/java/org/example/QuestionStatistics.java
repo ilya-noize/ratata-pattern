5dc4d229993f5494e9bbcc8233fdc23938648ae2
Ilya Shumakov <shumakoff.ilya@yandex.ru>
2026-01-30 21:49:29 +0300

    fix: Strategy

--- strategy/src/main/java/org/example/QuestionStatistics.java
+++ strategy/src/main/java/org/example/QuestionStatistics.java
@@ -6,4 +6,37 @@ public class QuestionStatistics {
     private String questionTitle;
     private Map<String,Integer> selectedVariantsCount;
     private Map<String,Integer> userSelectedVariantsCount;
+
+    protected QuestionStatistics() {
+    }
+
+    public QuestionStatistics(String questionTitle, Map<String, Integer> selectedVariantsCount, Map<String, Integer> userSelectedVariantsCount) {
+        this.questionTitle = questionTitle;
+        this.selectedVariantsCount = selectedVariantsCount;
+        this.userSelectedVariantsCount = userSelectedVariantsCount;
+    }
+
+    public String questionTitle() {
+        return questionTitle;
+    }
+
+    public void setQuestionTitle(String questionTitle) {
+        this.questionTitle = questionTitle;
+    }
+
+    public Map<String, Integer> selectedVariantsCount() {
+        return selectedVariantsCount;
+    }
+
+    public void setSelectedVariantsCount(Map<String, Integer> selectedVariantsCount) {
+        this.selectedVariantsCount = selectedVariantsCount;
+    }
+
+    public Map<String, Integer> userSelectedVariantsCount() {
+        return userSelectedVariantsCount;
+    }
+
+    public void setUserSelectedVariantsCount(Map<String, Integer> userSelectedVariantsCount) {
+        this.userSelectedVariantsCount = userSelectedVariantsCount;
+    }
 }
